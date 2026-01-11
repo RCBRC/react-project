3838c22c6b5f4f2118a85ecc257345da82d0d86b
sumit sharma <susharma@891@gmail.com>
2026-01-11 22:26:15 +0530

    feat: Add authentication and post management features
    - Implemented Protected route component for authentication checks.
    - Created reusable Button, Input, and Select components.
    - Developed Header and Footer components for consistent layout.
    - Added Login and Signup pages with form validation.
    - Created PostForm component for adding and editing posts.
    - Implemented AllPosts and Post components for displaying posts.
    - Integrated Redux for authentication state management.
    - Set up Appwrite service for backend interactions.
    - Configured Vite with Tailwind CSS for styling.
    - Established routing with React Router for navigation.

---
+++ 12MegaBlog/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function EditorJs() {
+  return (
+    <Editor
+      apiKey='o7ga7n7p0776zm6d32kpo62abeiovol1ga665hwlsr5thyy7'
+      init={{
+        plugins: [
+          // Core editing features
+          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+          // Your account includes a free trial of TinyMCE premium features
+          // Try the most popular premium features until Jan 25, 2026:
+          'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'ai', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+        ],
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+        uploadcare_public_key: '4a76c3630a87f28704cd',
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
+  );
+}
---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+import { Editor } from "@tinymce/tinymce-react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
+
+export default App;
